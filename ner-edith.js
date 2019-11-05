98bb7e407a409967492743695cdcc8943bd8e6ba
cneud <clemens.neudecker@gmail.com>
2019-11-05 20:05:45 +0100

    add entity tag B-ART for artworks

--- ner-edith.js
+++ ner-edith.js
@@ -92,6 +92,8 @@ function setupInterface(data, file) {
         $("#table td:contains('I-PUB')").addClass('ner_pub');
         $("#table td:contains('B-CONF')").addClass('ner_conf');
         $("#table td:contains('I-CONF')").addClass('ner_conf');
+        $("#table td:contains('B-ART')").addClass('ner_art');
+        $("#table td:contains('I-ART')").addClass('ner_art');
         $("#table td:contains('B-TODO')").addClass('ner_todo');
         $("#table td:contains('I-TODO')").addClass('ner_todo');
     }
@@ -268,6 +270,7 @@ function setupInterface(data, file) {
                         <div class="ner_org type_select">B-ORG</div>
                         <div class="ner_pub type_select">B-PUB</div>
                         <div class="ner_conf type_select">B-CONF</div>
+                        <div class="ner_art type_select">B-ART</div>
                         <div class="ner_todo type_select">B-TODO</div>
                     </div>
                 </section>
@@ -278,6 +281,7 @@ function setupInterface(data, file) {
                         <div class="ner_org type_select">I-ORG</div>
                         <div class="ner_pub type_select">I-PUB</div>
                         <div class="ner_conf type_select">I-CONF</div>
+                        <div class="ner_art type_select">I-ART</div>
                         <div class="ner_todo type_select">I-TODO</div>
                     </div>
                 </section>
